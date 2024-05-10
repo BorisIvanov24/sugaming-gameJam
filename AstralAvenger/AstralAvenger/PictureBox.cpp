fa51c9791edf64e1adcd0fc9ad8df1fe20b205de
BorisIvanov24 <boris.ivanov.go@gmail.com>
2024-05-11 02:29:23 +0300

    Added PictureBox

---
+++ AstralAvenger/AstralAvenger/PictureBox.cpp
@@ -0,0 +1,63 @@
+#include "PictureBox.h"
+
+PictureBox::PictureBox(const Texture2D &texture, const Rectangle &rectangle)
+{
+    this->texture = texture;
+    this->rectangle = rectangle;
+
+    resizeTexture();
+}
+
+PictureBox::PictureBox(const char *fileName, const Rectangle &rectangle)
+{
+    texture = LoadTexture(fileName);
+    this->rectangle = rectangle;
+    resizeTexture();
+}
+
+void PictureBox::loadTexture(const char *fileName)
+{
+    texture = LoadTexture(fileName);
+    resizeTexture();
+}
+
+void PictureBox::setTexture(const Texture2D &texture)
+{
+    this->texture = texture;
+    resizeTexture();
+}
+
+void PictureBox::setRectangle(const Rectangle &rectangle)
+{
+    this->rectangle = rectangle;
+    resizeTexture();
+}
+
+const Rectangle &PictureBox::getRectangle() const
+{
+    return rectangle;
+}
+
+const Texture2D &PictureBox::getTexture2D() const
+{
+    return texture;
+}
+
+void PictureBox::draw(const Color &tint) const
+{
+    DrawTexture(texture, rectangle.x, rectangle.y, tint);
+}
+
+void PictureBox::draw(const Vector2& position, const Color& tint) const
+{
+    DrawTexture(texture, position.x, position.y, tint);
+}
+
+void PictureBox::resizeTexture()
+{
+    Image img = LoadImageFromTexture(texture);
+
+    ImageResize(&img, rectangle.width, rectangle.height);
+
+    texture = LoadTextureFromImage(img);
+}
---
+++ AstralAvenger/AstralAvenger/PictureBox.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "TextBox.h"
+#include "raylib.h"
+
+class PictureBox
+{
+  public:
+    PictureBox() = default;
+    PictureBox(const Texture2D &texture, const Rectangle &rectangle);
+    PictureBox(const char *fileName, const Rectangle &rectangle);
+
+    void loadTexture(const char *fileName);
+    void setTexture(const Texture2D &texture);
+    void setRectangle(const Rectangle &rectangle);
+
+    const Rectangle &getRectangle() const;
+    const Texture2D &getTexture2D() const;
+
+    void draw(const Color &tint) const;
+    void draw(const Vector2& position, const Color& tint) const;
+
+  private:
+    void resizeTexture();
+
+    Texture2D texture = {0};
+    Rectangle rectangle = DEFAULT_REC;
+    Vector2 position = { 0 };
+};
